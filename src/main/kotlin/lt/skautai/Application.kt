9526f8a2cdd4a4fbdb1dbd8898d511afa33d0629
Augustas <augustas.koko@gmail.com>
2026-03-23 14:31:37 +0200

    PostreSQL connection added

--- src/main/kotlin/lt/skautai/Application.kt
+++ src/main/kotlin/lt/skautai/Application.kt
@@ -1,15 +1,35 @@
 package lt.skautai
 
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.transaction
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        module()
-    }.start(wait = true)
+fun main(args: Array<String>) {
+    EngineMain.main(args)
 }
 
 fun Application.module() {
+    configureDatabases()
+}
 
+fun Application.configureDatabases() {
+    val config = environment.config
+    val url = config.property("database.url").getString()
+    val driver = config.property("database.driver").getString()
+    val user = config.property("database.user").getString()
+    val password = config.property("database.password").getString()
+
+    Database.connect(
+        url = url,
+        driver = driver,
+        user = user,
+        password = password
+    )
+
+    val logger = log
+    transaction {
+        exec("SELECT 1")
+        logger.info("Database connection successful")
+    }
 }
